c620a0fc060b659238af9a326935262fe18bac5f
Jose Ferreira da Silva <ferreirajosejp@gmail.com>
2021-06-06 13:02:49 -0300

    add verb post from categorie

--- src/main/java/com/jose/cursomc/resources/CategoriasResource.java
+++ src/main/java/com/jose/cursomc/resources/CategoriasResource.java
@@ -1,14 +1,18 @@
 package com.jose.cursomc.resources;
 
+import java.net.URI;
+
 import com.jose.cursomc.domain.Categoria;
 import com.jose.cursomc.services.CategoriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController // Mapeando class  controller
 @RequestMapping(value="/categorias") // Mapendo rota para acesssar 
@@ -37,7 +41,15 @@ public class CategoriasResource {
 		Categoria cat = service.buscar(id);
 
 		return ResponseEntity.ok().body(cat);
-
+	}
 	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
